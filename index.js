da22365ef2a59cbd79b0b698bcd896c662a63b6a
tim mckiernan <tim@pursuit.org>
2022-06-12 09:48:31 -0400

    changes to person

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function createPerson(name, age) {
     };
   }
   
-  function addPersonAsFriend(person, friend) {
-    person.friends.push(friend);
+  function addPersonAsFriend(person, somePerson) {
+    person.friends.push(somePerson);
     return person;
   }
   
